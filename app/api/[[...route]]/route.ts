2c03e1339a9ca3ce0c4b02d81d78ac7c257ce7a6
Daddyjohn63 <john@webandprosper.co.uk>
2024-05-31 15:17:32 +0100

    transaction endpoints added.

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -1,14 +1,20 @@
 import { Hono } from 'hono';
 import { handle } from 'hono/vercel';
+
+//import summary from './summary';
 import accounts from './accounts';
 import categories from './categories';
+import transactions from './transactions';
 
 export const runtime = 'edge';
 
-//will overwrite next js file based writing to use Hono.
 const app = new Hono().basePath('/api');
 
-const routes = app.route('/accounts', accounts).route('/categories', categories);
+const routes = app
+  //.route('/summary', summary)
+  .route('/accounts', accounts)
+  .route('/categories', categories)
+  .route('/transactions', transactions);
 
 export const GET = handle(app);
 export const POST = handle(app);
