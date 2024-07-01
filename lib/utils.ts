aa55841d3e094e9e2351cff5d60afdb717504d30
Daddyjohn63 <john@webandprosper.co.uk>
2024-07-01 10:57:39 +0100

    summary API finished

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,5 +1,6 @@
 import { type ClassValue, clsx } from 'clsx';
 import { twMerge } from 'tailwind-merge';
+import { eachDayOfInterval, isSameDay } from 'date-fns';
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
@@ -19,4 +20,49 @@ export function formatCurrency(value: number) {
     currency: 'USD',
     minimumFractionDigits: 2
   }).format(value);
+}
+
+export function calculatePercentageChange(current: number, previous: number) {
+  if (previous === 0) {
+    return previous === current ? 0 : 100;
+  }
+
+  return ((current - previous) / previous) * 100;
+}
+
+export function fillMissingDays(
+  activeDays: {
+    date: Date;
+    income: number;
+    expenses: number;
+  }[],
+  startDate: Date,
+  endDate: Date
+) {
+  if (activeDays.length === 0) {
+    return [];
+  }
+  //generate a list of all days between start date and end date.
+  const allDays = eachDayOfInterval({
+    start: startDate,
+    end: endDate
+  });
+  //For each day in the generated interval:
+  //The transactionsByDay attempts to find the corresponding day in the activeDays array.
+  //If found, it includes the existing data for that day.
+  //If not found, it creates a new object for that day with zero income and expenses.
+  const transactionsByDay = allDays.map(day => {
+    const found = activeDays.find(d => isSameDay(d.date, day));
+
+    if (found) {
+      return found;
+    } else {
+      return {
+        date: day,
+        income: 0,
+        expenses: 0
+      };
+    }
+  });
+  return transactionsByDay;
 }
